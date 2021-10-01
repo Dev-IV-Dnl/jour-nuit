d1823a7487115cde70dd3c53539838840e4c7dac
Dev-IV-Dnl <mattgiordanengo@gmail.com>
2021-10-01 14:40:10 +0200

    maj

--- js/index.js
+++ js/index.js
@@ -1,11 +1,16 @@
 let jn = document.querySelector(".jour-nuit");
 let body = document.body;
+let cercle = document.querySelector(".cercleJN");
+
+
 jn.addEventListener("click", () => {
     if (body.classList.contains("light")) {
         body.classList.remove("light");
         body.classList.add("dark");
+        cercle.style.transform = "translate(23px, 0px)";
     } else if (body.classList.contains("dark")) {
         body.classList.remove("dark");
         body.classList.add("light");
+        cercle.style.transform = "translate(0, 0px)";
     }
-})
+});
